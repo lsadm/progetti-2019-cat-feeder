47de39149d54175b54eba97d90e6aa68a91ece87
giulio <giulio.luongo@studenti.unicampania.it>
2019-06-28 18:02:56 +0200

    Abbiamo aggiunto completamente un animale alla lista su firebase -nome -età -peso -razza -vaccinato (true/false) -sterilizzato (true/false)

--- app/src/main/java/com/greenjackets/prototipo/AddFragment.kt
+++ app/src/main/java/com/greenjackets/prototipo/AddFragment.kt
@@ -19,11 +19,13 @@ import java.util.jar.Manifest
 
 
 class AddFragment : Fragment() {
+    val QRCODE : Int=5    // TODO: aggiungere qrcode variabile
 
     val REQUEST_IMAGE_CAPTURE = 1 // serve per la fotocamera
     val firebaseDatabase = FirebaseDatabase.getInstance()       //Per accedere a firebase
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
@@ -36,8 +38,11 @@ class AddFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // Imposta il funzionamento del pulsante per l'acqisizione dell'immagine
+
+        val ref = firebaseDatabase.getReference("Utente/"+QRCODE)
+
         btn_fotocamera.setOnClickListener {
+            // Imposta il funzionamento del pulsante per l'acqisizione dell'immagine
             // Creo un intent di tipo implicito per acquisire l'immagine
             val takePhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             takePhoto.resolveActivity(activity!!.packageManager)?.also {
@@ -48,11 +53,30 @@ class AddFragment : Fragment() {
 
         //Bottone per aggiungere elementi su firebase
         btn_aggiungi.setOnClickListener {
-            val testo = txt_nome.text.toString()
+            val nome = txt_nome.text.toString()
+            val età = txt_eta.text.toString()
+            val peso = txt_peso.text.toString()
+            val razza = txt_razza.text.toString()
+            val checkvacc = check_vaccino.isChecked
+            val checkster= check_sterile.isChecked
+
+            ref.child("Nome").setValue(nome)
+            ref.child("Età").setValue(età)
+            ref.child("peso").setValue(peso)
+            ref.child("razza").setValue(razza)
+            ref.child("Vaccinato").setValue(checkvacc)
+            ref.child("Sterilizzato").setValue(checkster)
+
+
+
             //devo scrivere il contenuto nel database
-            val nodo = "nome"
-            val ref = firebaseDatabase.getReference("Utente")
-            ref.child("QR_CODE").child("Nome").setValue(testo)
+
+
+
+
+
+
+
 
         }
     }
