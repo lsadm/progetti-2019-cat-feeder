0f827c251c627f06b726995a956adbae3cd60726
heisengit <simone.cristofaro@studenti.unicampania.it>
2019-07-11 12:58:04 +0200

    Implementate tutte le funzionalità con il qrcode (funzionante)

--- app/src/main/java/com/greenjackets/prototipo/QRcodeFragment.kt
+++ app/src/main/java/com/greenjackets/prototipo/QRcodeFragment.kt
@@ -25,10 +25,15 @@ import com.journeyapps.barcodescanner.CaptureActivity
 import kotlinx.android.synthetic.main.fragment_add.*
 import java.io.FileInputStream
 import java.lang.Exception
+import java.lang.Thread.sleep
+import java.util.concurrent.locks.ReentrantLock
 
 
 class QRcodeFragment : Fragment() {
 
+    var foundDatabse=false
+    var foundLocal=false
+    var attesa=0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,13 +46,11 @@ class QRcodeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
             val scanner = IntentIntegrator.forSupportFragment(this)
             scanner.setPrompt("Scansiona il codice QR posto sulla ciotola!")
             scanner.setOrientationLocked(false)
            // scanner.setBarcodeImageEnabled(true)
-            scanner.setCameraId(0)
+            //scanner.setCameraId(0)
             scanner.initiateScan()
 
 
@@ -55,87 +58,67 @@ class QRcodeFragment : Fragment() {
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        var bool2: Boolean?=true
+
+
 
         if(resultCode == Activity.RESULT_OK){
-
             val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-
             if (result != null) {
-
                 if (result.contents == null) {
                     Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show()
                 } else {
                     Toast.makeText(context, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
 
-                    //dobbiamo controllare se il qrcode è stato già utilizzato sul database!
-                    val database= FirebaseDatabase.getInstance().getReference(result.contents.toString()) //reference al database
-                    var bool=false // deve variare quindi var
+                    /** Verifico se il QRCODE è gia memorizzato in locale
+                     */
+
                     val filename="Qrcodes.txt"
                     var filestream = context?.openFileInput(filename)
                     var bufferedreader =filestream?.bufferedReader()
-
-
-
-
-                        //dichiaro quello che deve fare il valueeventlistener
-                    val qrListener = object : ValueEventListener {  // creazione ValueEventListener
-                        override fun onDataChange(dataSnapshot: DataSnapshot) {
-                            val  animale = dataSnapshot.getValue(Animale::class.java)
-                            try {
-                                if(animale?.qrcode==null)
-                                    Toast.makeText(context, "Personalizza il profilo :)", Toast.LENGTH_LONG).show()
-                                if(animale?.qrcode==result.contents.toString()){
-
-                                    // se l'animale già esiste allora devo solo scrivere sul file il QRCODE già scannerizzato
-                                    val filename="Qrcodes.txt"
-                                    val fileContents = result.contents.toString()+"\n" // Gli dico di scrivere nel file il QR
-                                    context?.openFileOutput(filename, Context.MODE_APPEND).use {
-                                        it?.write(fileContents.toByteArray()) // uso openFileOutput
-                                    }
-                                    //SCRITTO SU FILE il QRCODES, così quando torno a home_frament lo ricarica!
-                                    Toast.makeText(context, "L'animale è già presente sul database", Toast.LENGTH_LONG).show()
-                                    bool2=false
-                                    Navigation.findNavController(view!!).navigateUp()
-                                }
-                            } catch (e: Exception) {}
-                        }
-                        override fun onCancelled(databaseError: DatabaseError) {
-                            // Getting Post failed, log a message
-                            Log.w(ContentValues.TAG, "loadPost:onCancelled", databaseError.toException())
-                        }
-                    }//Definizione ValueEventListener!
-
-
-
 
                     //Controllo se il qrcode è già stato scritto su file. Se è già scritto su file allora non devo controllare
                     //se sta sul database. Sicuro ci sarà!
                     bufferedreader?.forEachLine {
                         if(result.contents.toString()==it) {
-                            bool = true // se trovo il qrcode già scritto metto true
-                            bool2=false
-                            Toast.makeText(context, "Trovato sul locale QRCODEFRAG", Toast.LENGTH_LONG).show()
-                            Navigation.findNavController(view!!).navigateUp()
+                            foundLocal = true // se trovo il qrcode già scritto metto true
+                            Toast.makeText(context, "QRCODE già scannerizzato, se vuoi puoi modificarlo", Toast.LENGTH_LONG).show()
+                            Navigation.findNavController(view!!).navigate(R.id.action_QRcodeFragment_to_homeFragment)
                         }
                     }
 
-                    //chiamata alla lettura del database se non è presente il valore in Qrcodes.txt
-                    if(bool==false)
-                    database.addListenerForSingleValueEvent(qrListener)
-                    // una volta chiamato il addListener, se è presente il valore allora non prosegue nel addFragment, altrimenti deve tornare
-                    //indietro
+                    /** Verifico se il QRCODE è presente sul database
+                     */
+                    if(foundLocal==false) {
+                        val database= FirebaseDatabase.getInstance().getReference(result.contents.toString()) //reference al database
 
+                        database.addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                val  animale = dataSnapshot.getValue(Animale::class.java)
+                                try{
+                                    if(animale?.qrcode==result.contents.toString()){
+                                        // se l'animale già esiste allora devo solo scrivere sul file il QRCODE già scannerizzato
+                                        val filename="Qrcodes.txt"
+                                        val fileContents = result.contents.toString()+"\n" // Gli dico di scrivere nel file il QR
+                                        context?.openFileOutput(filename, Context.MODE_APPEND).use {
+                                            it?.write(fileContents.toByteArray()) // uso openFileOutput
+                                        }
+                                        //SCRITTO SU FILE il QRCODES, così quando torno a home_frament lo ricarica!
+                                        Toast.makeText(context, "L'animale è già presente sul database, lo scarico", Toast.LENGTH_LONG).show()
+                                        Navigation.findNavController(view!!).navigate(R.id.action_QRcodeFragment_to_homeFragment)
 
-                    //    Passaggio dati al fragment successivo
-                    if(bool2==true) {
-                        val b = Bundle()
-                        b.putString("qrcode", result.contents)
-                        b.putString(
-                            "Controllo",
-                            "Qrcodex"
-                        )    //TODO: Il nome dell'ogggetto andrebbe inserito in un solo punto!
-                        Navigation.findNavController(view!!).navigate(R.id.action_QRcodeFragment_to_addFragment, b)
+                                    }
+                                    else{
+                                            val b = Bundle()
+                                            b.putString("qrcode", result.contents)
+                                            b.putString("Controllo","Qrcodex")
+                                            Navigation.findNavController(view!!).navigate(R.id.action_QRcodeFragment_to_addFragment, b)
+                                    }
+                                } catch (e: Exception){}
+                            }
+
+                            override fun onCancelled(databaseError: DatabaseError) {
+                            }
+                        })
                     }
                 }
             } else {
@@ -145,14 +128,11 @@ class QRcodeFragment : Fragment() {
 
         if(resultCode == Activity.RESULT_CANCELED)
         {
-            Toast.makeText(context, "Annullato...", Toast.LENGTH_LONG).show()
+            Toast.makeText(context, "Annullato", Toast.LENGTH_LONG).show()
             Navigation.findNavController(view!!).navigate(R.id.action_QRcodeFragment_to_homeFragment)
         }
 
     }
-
-
-
 
 
 }
